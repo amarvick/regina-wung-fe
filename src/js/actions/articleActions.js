bb4148e2988ba0bd8870c42b9867d56348da57ea
Alex Marvick <amarvick94@gmail.com>
2021-03-14 17:25:04 -0700

    articleActions

--- src/js/actions/articleActions.js
+++ src/js/actions/articleActions.js
@@ -5,9 +5,6 @@ import data from '../../data/data.json';
 export function getArticles() {
   return function action(dispatch) {
     dispatch({
-      type: ArticleActionTypes.FETCHING_ARTICLES
-    })
-    dispatch({
       type: ArticleActionTypes.FETCH_ARTICLES_SUCCESS,
       payload: data
     })
